55fdcc3f3b1dfb1ef8fad3b791b51e3ce2fec6be
PhongNT <phongnt@kaopiz.com>
2023-11-07 15:47:04 +0700

    booking action

--- authenticate.php
+++ authenticate.php
@@ -9,9 +9,9 @@ include 'connect.php' ;
 include 'head2.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
-$sql_userdatabase = "Select * from userdatabase where email = '$email' and password='$password' ";
-$result_userdatabase = mysqli_query($connect, $sql_userdatabase);
-if(mysqli_num_rows($result_userdatabase) <= 0)
+$sql_users = "Select * from users where email = '$email' and password='$password' ";
+$result_users = mysqli_query($connect, $sql_users);
+if(mysqli_num_rows($result_users) <= 0)
 {
 		echo "<center><h1><b>Something went wrong please login again<b> </h1></center><br><br>";
 echo '<center><table><tr><td><A href="sindex.php"><button style="background-color:black; border-color:black"><span style="color:white">Sign in ! &nbsp&nbspAgain</span></button></a></td></tr></table></center>';
@@ -20,14 +20,14 @@ else
 {
 	
 	header("location:book.php");
-	$row_userdatabase = mysqli_fetch_array($result_userdatabase);
+	$row_users = mysqli_fetch_array($result_users);
 	session_start();
 	$_SESSION['email']=$email;
-	$_SESSION['name']=$row_userdatabase['Name'];
-	$_SESSION['userid']=$row_userdatabase['UserID'];
-	$_SESSION['Gender']=$row_userdatabase['Gender'];
-	$_SESSION['dob']=$row_userdatabase['DoB'];
-    $_SESSION['phone']=$row_userdatabase['Phone'];
+	$_SESSION['name']=$row_users['Name'];
+	$_SESSION['userid']=$row_users['UserID'];
+	$_SESSION['Gender']=$row_users['Gender'];
+	$_SESSION['dob']=$row_users['DoB'];
+    $_SESSION['phone']=$row_users['Phone'];
 
 
 
