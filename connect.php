01a2991833bfa0b6b7c3fc2e263a5462f10a066c
PhongNT <phongnt@kaopiz.com>
2023-11-07 15:47:27 +0700

    connect db and db design file

--- connect.php
+++ connect.php
@@ -1,7 +1,6 @@
 <?php
 $hostname  = 'localhost';
 $username = 'root';
-$password='';
-$dbname = 'train';
-$connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
-?>
+$password = '';
+$dbname = 'ticket-booking';
+$connect =  mysqli_connect($hostname, $username, $password, $dbname) or die("Error Connecting");
