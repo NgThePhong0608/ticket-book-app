f6708a5336b500ffb411fdc30ace3845717a350d
PhongNT <phongnt@kaopiz.com>
2023-11-07 16:54:40 +0700

    dockerize app

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php
-$hostname  = 'localhost';
-$username = 'root';
-$password = '';
+$hostname  = 'localhost:8081' || 'localhost:8080';
+$username = 'php_docker';
+$password = 'password';
 $dbname = 'ticket-booking';
 $connect =  mysqli_connect($hostname, $username, $password, $dbname) or die("Error Connecting");
+mysqli_set_charset($connect, 'UTF8');
